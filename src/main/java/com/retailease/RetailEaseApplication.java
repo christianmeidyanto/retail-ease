1daf1f9a58fd0dc3362cc03c7b3a6dba63d3c438
Christian Meidyanto <meidyantoc@gmail.com>
2023-11-13 10:06:38 +0700

    Initial Commit

---
+++ src/main/java/com/retailease/RetailEaseApplication.java
@@ -0,0 +1,13 @@
+package com.retailease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailEaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailEaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/retailease/RetailEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retailease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailEaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
